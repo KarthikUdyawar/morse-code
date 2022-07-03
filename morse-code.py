249c8308276994353be8c4e434e767a6382b6bc0
Karthik Udyawar <karthikajitudy@gmail.com>
2022-07-03 16:30:48 +0530

    Update morse-code.py
    ### Fixed
    
    - Type casting

--- morse-code.py
+++ morse-code.py
@@ -1,11 +1,11 @@
 # import packages
 import winsound, time, io, pygame
 from gtts import gTTS
-import tkinter as tk
-from tkinter import messagebox
+import tkinter as Tk
+from tkinter import StringVar, messagebox
 
 # Morse code dictionary
-morse_code = {
+morse_code: "dict[str, str]" = {
     "": "  ",
     " ": "/",
     "\n": "//",
@@ -48,50 +48,50 @@ morse_code = {
 }
 
 # inverse dictionary
-textConverter = {value: key for (key, value) in morse_code.items()}
+textConverter: "dict[str, str]" = {value: key for (key, value) in morse_code.items()}
 
 # Variables
-root = tk.Tk()
+root: Tk = Tk.Tk()
 root.title("Morse Code")
-text_var = tk.StringVar()
-code_var = tk.StringVar()
+text_var: StringVar = Tk.StringVar()
+code_var: StringVar = Tk.StringVar()
 
 # Text to code
-def encoder():
+def encoder() -> None:
     code_display.config(state="normal")
-    code_display.delete("1.0", tk.END)
-    text = text_entry.get("1.0", "end-1c").upper()
-    code = ""
+    code_display.delete("1.0", Tk.END)
+    text: str = text_entry.get("1.0", "end-1c").upper()
+    code: str = ""
     try:
         for letter in text:
             code = f"{code} {morse_code[letter]}"
     except Exception:
         messagebox.showerror("Error", "Please enter valid text")
-    code_display.insert(tk.INSERT, code)
+    code_display.insert(Tk.INSERT, code)
     code_display.config(state="disabled")
 
 
 # Code to text
 def decoder():
     text_display.config(state="normal")
-    text_display.delete("1.0", tk.END)
-    code = code_entry.get("1.0", "end-1c")
-    groups = [i for j in code.split() for i in (j, "  ")]
-    text = ""
+    text_display.delete("1.0", Tk.END)
+    code: str = code_entry.get("1.0", "end-1c")
+    groups: list[str] = [i for j in code.split() for i in (j, "  ")]
+    text: str = ""
     try:
         for group in groups:
             text = text + textConverter[group]
     except Exception:
         messagebox.showerror("Error", "Please enter valid code")
-    text_display.insert(tk.INSERT, text)
+    text_display.insert(Tk.INSERT, text)
     text_display.config(state="disabled")
 
 
 # Code to beeps
 def sound():
-    code = code_display.get("1.0", "end-1c")
-    beeps = []
-    beeps[1:] = code
+    code: str = code_display.get("1.0", "end-1c")
+    beeps: list = []
+    beeps[1:]: list = code
 
     for beep in beeps:
         if beep == ".":
@@ -106,7 +106,7 @@ def sound():
 
 # Text to audio
 def speak():
-    text = text_display.get("1.0", "end-1c")
+    text: str = text_display.get("1.0", "end-1c")
     try:
         with io.BytesIO() as file:
             gTTS(text=text, lang="en").write_to_fp(file)
@@ -122,20 +122,20 @@ def speak():
 
 # Main function
 if __name__ == "__main__":
-    text_label = tk.Label(root, text="Enter text : ")
-    text_entry = tk.Text(root, width=50, height=5)
-    code_output = tk.Label(root, text="Encoded message : ")
-    code_display = tk.Text(root, width=50, height=5)
+    text_label = Tk.Label(root, text="Enter text : ")
+    text_entry = Tk.Text(root, width=50, height=5)
+    code_output = Tk.Label(root, text="Encoded message : ")
+    code_display = Tk.Text(root, width=50, height=5)
 
-    code_label = tk.Label(root, text="Enter code : ")
-    code_entry = tk.Text(root, width=50, height=5)
-    text_output = tk.Label(root, text="Decoded message : ")
-    text_display = tk.Text(root, width=50, height=5)
+    code_label = Tk.Label(root, text="Enter code : ")
+    code_entry = Tk.Text(root, width=50, height=5)
+    text_output = Tk.Label(root, text="Decoded message : ")
+    text_display = Tk.Text(root, width=50, height=5)
 
-    text_btn = tk.Button(root, text="Encode", command=encoder, width=50)
-    sound_btn = tk.Button(root, text="Beep", command=sound, width=50)
-    code_btn = tk.Button(root, text="Decode", command=decoder, width=50)
-    speak_btn = tk.Button(root, text="Audio", command=speak, width=50)
+    text_btn = Tk.Button(root, text="Encode", command=encoder, width=50)
+    sound_btn = Tk.Button(root, text="Beep", command=sound, width=50)
+    code_btn = Tk.Button(root, text="Decode", command=decoder, width=50)
+    speak_btn = Tk.Button(root, text="Audio", command=speak, width=50)
 
     text_label.grid(row=0, column=0, padx=2, pady=2)
     text_entry.grid(row=1, column=0, padx=2, pady=2)
